886f48625215498b6513069539b7174304281a01
Mohamed <M.Mahgoub04@gmail.com>
2022-06-04 21:34:15 +0400

    Functioning version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import { projectList, Todo } from './logic'
+import { projectList, Todo, setIndex } from './logic'
+import { format } from 'date-fns'
 
 function displayMenu() {
+ setIndex()
  const menu = document.createElement('select')
+ const menuDiv = document.querySelector('#menu-div')
+ menuDiv.innerHTML = '<p>Add task to: </p>'
  menu.setAttribute('id', 'menu')
   for (let i of projectList){
    const option = document.createElement('option')
@@ -9,100 +13,120 @@ function displayMenu() {
    option.setAttribute('value', i.index)
    menu.append(option)
   }
- document.querySelector('#add-task').append(menu)
-
+ menuDiv.append(menu)
 }
 
 function display() {
-  
-  
-  document.querySelector('#projects').innerHTML = ''
-  
-  
-  
-  for (let i of projectList){
-   const div = document.createElement('div')
-   const div2 = document.createElement('div')
-   const h3 = document.createElement('h3')
-   
+ displayMenu()
+ document.querySelector('#projects').innerHTML = ''
+ for (let i of projectList){
+  const div = document.createElement('div')
+  const div2 = document.createElement('div')
+  const h3 = document.createElement('h3')
 
-   div.setAttribute('data-index', i.index)
-   div.className = 'project'
-   h3.innerHTML = i.project
-   div2.className = 'tasks'
-   for (let j of i.tasks){
-    const div3 = document.createElement('div')
-    const p = document.createElement('p')
-    const img = document.createElement('img')
-
-    div3.className = 'task'
-    p.innerHTML = j.title
-    img.setAttribute('src', 'cross.png')
-
-    div3.append(p)
-    div3.append(img)
-    div2.append(div3)
+  // div.setAttribute('data-index', i.index)
+  div.className = 'project'
+  h3.innerHTML = i.project
+  div2.className = 'tasks'
+  for (let j of i.tasks){
+   const div3 = document.createElement('div')
+   const p = document.createElement('p')
+   const p2 = document.createElement('p')
+   const p3 = document.createElement('p')
+   const img = document.createElement('img')
+   div3.className = 'task flex'
+   div3.onclick = () => {
+    if (div3.className == 'task flex'){
+    div3.className = 'task grid'
+    div3.querySelector('.description').style.display = 'block'
+    // if (x.querySelector('.description').innerHTML == ''){
+    //  x.querySelector('.description').innerHTML = 'No description'
+    // }
+   } else {
+    div3.className = 'task flex'
+    div3.querySelector('.description').style.display = 'none'
    }
-   div.append(h3)
-   div.append(div2)
-   document.querySelector('#projects').append(div)
-   
-   
-   
+   }
+   p.className = 'title'
+   p.innerHTML = j.title
+   p2.className = 'description'
+   p2.innerHTML = j.description
+   p3.className = 'date'
+   p3.innerHTML = j.dueDate
+   img.setAttribute('src', 'cross.png')
+
+   div3.append(p)
+   div3.append(p2)
+   div3.append(p3)
+   div3.append(img)
+   div2.append(div3)
   }
-  document.querySelector('#add-task').append(menu)
-
-  // const plus = document.createElement('img')
-  // plus.setAttribute('src', 'cross.png')
-  // plus.setAttribute('id', 'plus')
-  const plus = document.createElement('p')
-  plus.innerHTML = '+'
-  plus.setAttribute('id', 'plus')
-  document.querySelector('#projects').append(plus)
-
+  div.append(h3)
+  div.append(div2)
+  document.querySelector('#projects').append(div)
  }
+
+ // Display plus sign
+ const plus = document.createElement('p')
+ plus.innerHTML = '+'
+ plus.setAttribute('id', 'plus')
+ plus.onclick = () => {
+  document.querySelector('#modal-bg').style.display = 'flex'
+ }
+ document.querySelector('#projects').append(plus)
+
+}
+
+function addProject() {
+ const modal = document.querySelector('#modal-bg')
+ const exit = modal.querySelector('img')
+
+ exit.onclick = () => {
+  modal.style.display = 'none'
+ }
+
+ modal.querySelector('button').onclick = () => {
+  let name = modal.querySelector('input')
+  let newProject = {
+   project: name.value,
+   tasks: []
+  }
+  projectList.push(newProject)
+  modal.style.display = 'none'
+  display()
+  console.log(projectList)
+ }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
  const input = document.querySelector('#add-task')
  const inpField = input.querySelector('input')
- 
+ const textarea = input.querySelector('textarea')
+ const date = document.querySelector('#calendar')
  const submit = input.querySelector('button')
- // const defaultProject = document.querySelector('#default')
- // const tasks = defaultProject.querySelector('.tasks')
- 
- displayMenu()
+ const tasks = document.querySelectorAll('.task')
+
  display()
- console.log(projectList)
+ addProject()
 
  submit.onclick = () =>{
-  const div = document.createElement('div')
-  const p = document.createElement('p')
-  const img = document.createElement('img')
   let task = inpField.value
+  let description = textarea.value
+  let dueDate = date.valueAsDate
   let optionVal = document.querySelector('select').value
+
+  dueDate = format(new Date(dueDate), "d MMM yyyy")
 
   for (let i of projectList){
    if (i.index == optionVal){
-
-    let todo = new Todo(task, '')
+    let todo = new Todo(task, description, dueDate)
     i.tasks.push(todo)
     display()
-    // alert()
-    // p.innerHTML = task
-    // img.setAttribute('src', 'cross.png')
-    // div.className = 'task'
-
-    // div.append(p)
-    // div.append(img)
-    // document.querySelector(`[data-index="${i.index}"]`).querySelector('.tasks').append(div)
-    // tasks.append(div)
-
    }
   }
   inpField.value = ''
+  textarea.value = ''
   console.log(projectList)
-  
-  
  }
 
 })
--- src/logic.js
+++ src/logic.js
@@ -1,47 +1,41 @@
-// function Todo(title, description, dueDate, priority){
-
-//  this.title = title
-//  this.description = description
-//  this.dueDate = dueDate
-//  this.priority = priority
-
-// }
-
 let projectList = [{
- index: 0,
  project: 'Default project',
  tasks: [
-  {title: 'Water the plants', description: ''},
-  {title: 'Feed the dog', description: ''},
-  {title: 'Do the dishes', description: ''}
+  {title: 'Water the plants', description: '', dueDate: '18 Jun 2022'},
+  {title: 'Feed the dog', description: 'Chicken breast and bone', dueDate: '18 Jun 2022'},
+  {title: 'Do the dishes', description: '', dueDate: '18 Jun 2022'}
  ]
-},{
- index: 1,
- project: 'Web development',
- tasks: [
-  {title: 'Learn react', description: ''},
-  {title: 'Learn npm and yarn', description: ''},
-  {title: 'Learn MongoDB', description: ''}
- ]
-}, {
- index: 2,
- project: 'University',
- tasks: [
-  {title: 'Study for biology midterm exam', description: ''},
-  {title: 'Prepare the chemistry presentation', description: ''}
- ]
+// },{
+//  project: 'Web development',
+//  tasks: [
+//   {title: 'Learn react', description: ''},
+//   {title: 'Learn npm and yarn', description: ''},
+//   {title: 'Learn MongoDB', description: ''}
+//  ]
+// }, {
+//  project: 'University',
+//  tasks: [
+//   {title: 'Study for biology midterm exam', description: ''},
+//   {title: 'Prepare the chemistry presentation', description: ''}
+//  ]
 }]
 
 class Todo {
- constructor(title, description){
+ constructor(title, description, dueDate, priority){
   // , dueDate, priority
   this.title = title
   this.description = description
-  // this.dueDate = dueDate
-  // this.priority = priority
+  this.dueDate = dueDate
+  this.priority = priority
  }
- 
-
 }
 
-export { projectList, Todo }
+function setIndex() {
+ let count = 0
+ for (let i of projectList){
+  i.index = count
+  count++
+ }
+}
+
+export { projectList, Todo, setIndex }
